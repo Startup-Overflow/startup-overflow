e2ba49b817a095e411b47e33d732dd153bce241a
aritra <aritrachowdhury65@gmail.com>
2022-05-04 13:08:03 +0530

    post title header section done

--- src/components/pages/PostTitle.js
+++ src/components/pages/PostTitle.js
@@ -1,6 +1,31 @@
 import "./PostTitle.css";
 
 function PostTitle() {
-  return <div className="post-title-new">post title</div>;
+  return (
+    <div className="post-title-new">
+      <div className="post-title-header">
+        <div className="header-title">Risk Breaking Concept</div>
+        <div className="posted-by">
+          Posted by,
+          <span className="posted-by-name">Aritra</span>
+          <div className="eventDateTime post-event-date">
+            <i class="fa fa-calendar"></i>
+            <span className="date">05/04/2022</span>
+            <i class="fa fa-clock"></i>
+            <span className="time">06:30 am IST</span>
+          </div>
+        </div>
+      </div>
+      <div className="post-short-description">
+        This is the premier and most popular concept. "entrepreneurship is a
+        function of taking unlimited risks." This has revealed that the
+        entrepreneur has to bear various types of risks for establishing the new
+        enterprise and operating it. Entrepreneurship as the concept capacity
+        bear uncertainties. Further, elaborating it they have made a slight
+        differentiation between risk and uncertainty. According to this,
+        "Uncertainty bear risk, which can neither be predicted nor insured."
+      </div>
+    </div>
+  );
 }
 export default PostTitle;
