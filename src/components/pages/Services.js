adad27bf335171c3e0f5c1eaea4ff4dac1197c16
Sudipta <hisudipta26@gmail.com>
2022-05-11 17:16:45 +0530

    some unsaved things

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -5,37 +5,34 @@ export default function Services() {
   return(
     <div>
       <h1 className='services_heading'>Our <span style={{color:'#14b8a6'}}>Services</span></h1>
-      <div className='box-container'>
-      <div class="box">
+      <div className='services-container'>
+      <div class="services">
             <i class="fa fa-calendar"></i>
             <h3>Events</h3>
-            <p>We are back in the post-pandemic world with a bouquet of hotels across Indian cities ranging from leisure to business ones with a new outlook.</p>
         </div>
         <div class="box">
             <i class="fa fa-calendar"></i>
-            <h3>Workshops & Sessions
-            </h3>
-            <p>we offer a range of shelves displaying glassware and bottles. A dining system where the guests serve themselves.</p>
+            <h3>Workshops</h3>
+        </div>
+        <div class="box">
+            <i class="fa fa-calendar"></i>
+            <h3>Sessions</h3>
         </div>
         <div class="box">
             <i class="fa fa-user-plus"></i>
             <h3>Mentorship</h3>
-            <p>Nothing can be better than looking at the world in a different way with our world tour packages from Indian cities.</p>
         </div>
         <div class="box">
             <i class="fa fa-users"></i>
             <h3>Partnership</h3>
-            <p>We provide patient transportation to a medical centre is no less challenging than treatment or checkup.</p>
         </div>
         <div class="box">
-            <i class="fa fa-usd" aria-hidden="true"></i>
+            <i class="fa fa-usd"></i>
             <h3>Investment</h3>
-            <p>We provide Adventurous travel which is a leisure activity that takes place in an unusual, exotic, remote or wilderness destination.</p>
         </div>
         <div class="box">
             <i class="fa fa-lightbulb"></i>
             <h3>Innovative ideas</h3>
-            <p>We provide Adventurous travel which is a leisure activity that takes place in an unusual, exotic, remote or wilderness destination.</p>
         </div>
       </div>
     </div>
