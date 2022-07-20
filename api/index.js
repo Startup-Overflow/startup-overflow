8bd1f7761240ff4f76877b39a4cd1a81af5bde4f
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-07-20 17:20:24 +0530

    ----

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Backend Server is running on port ${process.env.PORT}`);
-});
